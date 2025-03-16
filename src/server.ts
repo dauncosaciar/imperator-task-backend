3260f255676722d7ab31ea1f74ba8e753266a921
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-03-16 20:34:59 -0300

    create routing for Projects

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
+import projectRoutes from "./routes/projectRoutes";
 
 // Allow reading of .env files
 dotenv.config();
@@ -10,5 +11,8 @@ connectDB();
 
 // Create express application
 const app = express();
+
+// Api routes
+app.use("/api/projects", projectRoutes);
 
 export default app;
