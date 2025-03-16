07caa304bb5cc6dba9c09267b591376060efb1a9
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-03-16 17:48:31 -0300

    add explanatory comments

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
 
+// Allow reading of .env files
 dotenv.config();
 
+// Connect to database
 connectDB();
 
+// Create express application
 const app = express();
 
 export default app;
