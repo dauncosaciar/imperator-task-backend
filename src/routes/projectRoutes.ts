9728f521cecd220ab7ee31df100ad5817c47146b
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-03-17 10:54:43 -0300

    add a validation middleware

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,25 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middlewares/validation";
 import { ProjectController } from "../controllers/ProjectController";
 
 const router = Router();
 
-router.post("/", ProjectController.createProject);
+router.post(
+  "/",
+  body("projectName")
+    .notEmpty()
+    .withMessage("El Nombre del Proyecto es obligatorio"),
+  body("clientName")
+    .notEmpty()
+    .withMessage("El Nombre del Cliente es obligatorio"),
+  body("description")
+    .notEmpty()
+    .withMessage("La Descripción del Proyecto es obligatoria"),
+  handleInputErrors,
+  ProjectController.createProject
+);
+
 router.get("/", ProjectController.getAllProjects);
 
 export default router;
