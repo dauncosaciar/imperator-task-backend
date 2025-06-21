b8b5a22bf6e8669741329d059e3055a9948df344
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-06-21 16:52:06 -0300

    add validation to prevent duplicate user registration

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -5,12 +5,20 @@ import { hashPassword } from "../utils/auth";
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
-      const { password } = req.body;
+      const { email, password } = req.body;
+
+      // Prevent duplicated users
+      const userExists = await User.findOne({ email });
+
+      if (userExists) {
+        const error = new Error("El Usuario con ese email ya está registrado");
+        res.status(409).json({ error: error.message });
+        return;
+      }
+
+      // Create and save user
       const user = new User(req.body);
-
-      // Hash user password
       user.password = await hashPassword(password);
-
       await user.save();
       res.send("Cuenta creada. Revisa tu email para confirmarla");
     } catch (error) {
