3c4f72ef90061c3d2e559bdf6df4e22d2b532e83
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-07-03 15:47:50 -0300

    update href attribute in sendConfirmationEmail template

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -18,7 +18,7 @@ export class AuthEmail {
         <p>¡Hola, <b>${user.name} ${user.lastName}</b>!</p>
         <p>Has creado tu cuenta en <b><i>ImperatorTask</i></b>, ya casi está todo listo, sólo debes confirmarla.</p>
         <p>Visita el siguiente enlace:</p>
-        <a href="#">Confirmar Cuenta</a>
+        <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar Cuenta</a>
         <p>E ingresa el código: <b>${user.token}</b></p>
         <p>Este código expira en 5 minutos.</p>
       `
